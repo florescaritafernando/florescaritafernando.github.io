91bf997b17096468ae90f04631690b49277737b7
florescaritafernando <florescaritafernando@gmail.com>
2022-10-21 13:43:36 -0500

    cv update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".menu")
+const navMenu = document.querySelector(".menu");
+const direccion_menu = document.querySelector(".nav-menu-item");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("menu-visible")
+});
+
+direccion_menu.addEventListener("click", () =>{
+    navMenu.classList
 })
+
+
